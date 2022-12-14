cd6ff007ea47b42b3e66db2cea8bbf5b5a3b2306
jerzy <jerzy.hinc@gmx.com>
2022-12-14 19:01:58 +0100

    Working on producing combinations of dividing compressor assemblies

--- DividingCompressorFactory.py
+++ DividingCompressorFactory.py
@@ -3,6 +3,42 @@ from DummyCompressor import DummyCompressor
 from DummyDivider import DummyDivider
 from DummyEncancer import DummyEnhancer
 
+class AssemblyPosition:
+    def __init__(self, name, possibleAssembliesList) -> None:
+        self.name = name
+        self.nextPosition = None
+        self.possibleAssembliesList = possibleAssembliesList
+        self.currentIndex = 0
+        pass
+    def connectNextPosition(self, position):
+        if self.nextPosition == None:
+            self.nextPosition = position
+        else:
+            self.nextPosition.connectNextPosition(position)
+    def getNextPositions(self):
+        None
+
+    def getCuttentPositions(self):
+        None
+    
+    def hasNextPosition(self):
+        None
+
+class AssemblyCombiner:
+    def __init__(self, possibleAssembliesListsByName) -> None:
+        self.possibleAssembliesListsByName
+        pass
+
+    def connectPositions(self, possibleAssembliesListsByName) -> None:
+        possibleAssembliesListsByName = {}
+        self.firstAssembly = None
+        for name in possibleAssembliesListsByName:
+            newAssemblyPosition = AssemblyPosition(name, possibleAssembliesListsByName[name])
+            if self.firstAssembly == None:
+                self.firstAssembly = newAssemblyPosition
+            else:
+                self.firstAssembly.connectNextPosition(newAssemblyPosition)
+
 class DividingCompressorFactory:
     def __init__(self):
         pass
--- tests.m
+++ tests.m
@@ -1,1 +1,14 @@
-b = [ fir1(50,0.5,"high") ; fir1(50,0.5,"low")]
+%b = [ fir1(50,0.5,"high") ; fir1(50,0.5,"low")]
+
+[u, Fs] = audioread('diaboj.wav');
+N   = 4;
+G   = 10; % 10 dB
+Wo1 = 0;
+Wo2 = 1; % Corresponds to Fs/2 (Hz) or pi (rad/sample)
+BW = 1000/(Fs/2); % Bandwidth occurs at 7.4 dB in this case
+[B1,A1] = designParamEQ(N,G,Wo1,BW,'Orientation','row');
+[B2,A2] = designParamEQ(N,G,Wo2,BW,'Orientation','row');
+BQ1 = dsp.SOSFilter('Numerator',B1,'Denominator',A1);
+BQ2 = dsp.SOSFilter('Numerator',B2,'Denominator',A2);
+hfvt = fvtool(BQ1,BQ2,'Fs',Fs,'Color','white');
+legend(hfvt,'Low Shelf Filter','High Shelf Filter');
