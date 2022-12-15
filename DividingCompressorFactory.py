65863f8bd9c461e02b36c2608c4f0c9c14a6ba9d
jerzy <jerzy.hinc@gmx.com>
2022-12-15 22:09:55 +0100

    early concept of producing all combinations of dividing compressor

--- DividingCompressorFactory.py
+++ DividingCompressorFactory.py
@@ -1,9 +1,10 @@
 from DividingCompressor import DividingCompressor
 from DummyCompressor import DummyCompressor
-from DummyDivider import DummyDivider
 from HighbandBooster import HighbandBooster
 from G7231Compressor import G7231Compressor
+from DummyEnhancer import DummyEnhancer
 
+from QMFDivider import QMFDivider
 from QMFWithHighbandModDivider import QMFWithHighbandModDivider
 
 import matlab.engine
@@ -13,36 +14,63 @@ class AssemblyPosition:
         self.name = name
         self.nextPosition = None
         self.possibleAssembliesList = possibleAssembliesList
-        self.currentIndex = 0
+        self.currentIndex = -1
         pass
     def connectNextPosition(self, position):
         if self.nextPosition == None:
             self.nextPosition = position
         else:
             self.nextPosition.connectNextPosition(position)
-    def getNextPositions(self):
-        None
 
-    def getCuttentPositions(self):
+    def moveToNext(self):
+        if self.currentIndex + 1 < len(self.possibleAssembliesList):
+            self.currentIndex += 1
+        elif self.nextPosition != None:
+            self.currentIndex = 0
+            self.nextPosition.moveToNext()
+        else:
+            raise IndexError()
+
+    def getNextPositions(self):
+        self.moveToNext()
+        self.getCurrentPositions()
         None
     
+    def getCurrentPositions(self):
+        if self.nextPosition == None:
+            return [self.possibleAssembliesList[self.currentIndex]]
+        else:
+            return self.nextPosition.getCurrentPositions.append(self.possibleAssembliesList[self.currentIndex])
+
     def hasNextPosition(self):
-        None
+        if self.currentIndex + 1 < len(self.possibleAssembliesList):
+            return True
+        elif self.nextPosition != None:
+            return self.nextPosition.hasNextPosition
+        else:
+            return False
+            
 
 class AssemblyCombiner:
     def __init__(self, possibleAssembliesListsByName) -> None:
         self.possibleAssembliesListsByName
+        self.connectPositions()
         pass
 
-    def connectPositions(self, possibleAssembliesListsByName) -> None:
-        possibleAssembliesListsByName = {}
+    def connectPositions(self) -> None:
         self.firstAssembly = None
-        for name in possibleAssembliesListsByName:
-            newAssemblyPosition = AssemblyPosition(name, possibleAssembliesListsByName[name])
+        for name in self.possibleAssembliesListsByName:
+            newAssemblyPosition = AssemblyPosition(name, self.possibleAssembliesListsByName[name])
             if self.firstAssembly == None:
                 self.firstAssembly = newAssemblyPosition
             else:
                 self.firstAssembly.connectNextPosition(newAssemblyPosition)
+
+    def getNextCombination(self):
+        return self.firstAssembly.getNextPositions()
+
+    def hasNextCombination(self):
+        return self.firstAssembly.hasNextPosition()
 
 class DividingCompressorFactory:
     def __init__(self):
@@ -50,19 +78,41 @@ class DividingCompressorFactory:
         pass
     def generate(self):
         generatedCompressors = []
-        # dummyDivider = DummyDivider()
-        divider = QMFWithHighbandModDivider(self.mlEngine)
 
+        dividers = [QMFDivider(self.mlEngine), QMFWithHighbandModDivider(self.mlEngine)]
+        enhancers = [DummyEnhancer(), HighbandBooster()]
+        highBandCompressors = [DummyCompressor()]
+        lowBandCompressors = [DummyCompressor()]
+        
+        G7231Compressors = []
         G7231Compressor1 = G7231Compressor(self.mlEngine)
-        G7231Compressor1.outputFileName = 'G7231_1_output.wav'
-        G7231Compressor1.reconstuctionFileName = 'G7231_1_reconst.wav'
+        G7231Compressors.append(G7231Compressor1)
+
+        G7231Compressor1 = G7231Compressor1.getCopy()
         G7231Compressor1.isUsingCustomImpulseNumber = True
         G7231Compressor1.impulseNumber = 8
-        G7231Compressor2 = G7231Compressor(self.mlEngine)
-        G7231Compressor2.outputFileName = 'G7231_2_output.wav'
-        G7231Compressor2.reconstuctionFileName = 'G7231_2_reconst.wav'
-        G7231Compressor2.isUsingSeparateExcitation = True
-        G7231Compressor2.impulseNumber = 10
+        G7231Compressors.append(G7231Compressor1)
+
+        G7231Compressor1 = G7231Compressor1.getCopy()
+        G7231Compressor1.impulseNumber = 16
+        G7231Compressors.append(G7231Compressor1)
+        
+        G7231Compressor1 = G7231Compressor1.getCopy()
+        G7231Compressor1.impulseNumber = 32
+        G7231Compressors.append(G7231Compressor1)
+
+        G7231Compressor1 = G7231Compressor1.getCopy()
+        G7231Compressor1.impulseNumber = 64
+        G7231Compressors.append(G7231Compressor1)
+
+        for c in G7231Compressors:
+            copy = c.getCopy()
+            copy.isUsingSeparateExcitation = True
+            .......................
+
+        # dummyDivider = DummyDivider()
+        # divider = QMFWithHighbandModDivider(self.mlEngine)
+
         bandCompressorsList = [G7231Compressor1, G7231Compressor2]
         highbandBooster = HighbandBooster(self.mlEngine, 10)
         generatedCompressors.append(DividingCompressor(divider, highbandBooster, bandCompressorsList))
--- G7231Compressor.py
+++ G7231Compressor.py
@@ -23,10 +23,10 @@ class G7231Compressor(BandCompressorBase):
     def compress(self, signalFileName):
         #signal = matlab.double(signal)
         #self.engine.wavwrite(signal, self.samplongFreq, self.inputFileName, nargout=0)
-        if self.isUsingCustomImpulseNumber:
+        if self.isUsingSeparateExcitation:
             self.engine.G7231Coder_imp(signalFileName, self.outputFileName, self.impulseNumber, nargout=0)
 
-        elif self.isUsingSeparateExcitation:
+        elif self.isUsingCustomImpulseNumber:
             self.engine.G7231Coder_mp(signalFileName, self.outputFileName, self.impulseNumber, nargout=0)
 
         else:
@@ -40,3 +40,13 @@ class G7231Compressor(BandCompressorBase):
 
     def __repr__(self):
         return "G7231Compressor"
+
+    def getCopy(self) -> G7231Compressor:
+        copy = G7231Compressor(self.engine)
+        copy.impulseNumber = self.impulseNumber
+        copy.isUsingCustomImpulseNumber = self.isUsingCustomImpulseNumber
+        copy.impulseNumber = self.impulseNumber
+        copy.isUsingSeparateExcitation = self.isUsingSeparateExcitation
+        copy.outputFileName = self.outputFileName
+        copy.reconstuctionFileName = self.reconstuctionFileName
+        return copy
